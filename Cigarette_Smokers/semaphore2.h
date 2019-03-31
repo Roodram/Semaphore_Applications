2f7df43559984d172860cd2d6b8404bc8f69c990
Roodram <34090275+Roodram@users.noreply.github.com>
2019-03-31 11:08:19 +0530

    Update semaphore2.h

--- Cigarette_Smokers/semaphore2.h
+++ Cigarette_Smokers/semaphore2.h
@@ -5,9 +5,9 @@
 #include<pthread.h>
 
 struct semaphore{
-	int val;
+	int sem_val;
 	pthread_mutex_t mutex;
-	pthread_cond_t cond;
+	pthread_cond_t condition;
 };
 
 semaphore s;
@@ -15,24 +15,24 @@ semaphore s;
 void semaphore_wait(semaphore *s)
 {
 	pthread_mutex_lock(&(s->mutex));
-	while (s->val == 0) {
-		pthread_cond_wait(&(s->cond), &(s->mutex));
+	while (s->sem_val == 0) {
+		pthread_cond_wait(&(s->condition), &(s->mutex));
 	}
-	s->val--;
+	s->sem_val--;
 	pthread_mutex_unlock(&(s->mutex));
 }
 
 void semaphore_signal(semaphore *s)
 	{
 	pthread_mutex_lock(&(s->mutex));
-	s->val++;
-	pthread_cond_broadcast(&(s->cond));
+	s->sem_val++;
+	pthread_cond_broadcast(&(s->condition));
 	pthread_mutex_unlock(&(s->mutex));
 }
 
 void semaphore_init(semaphore *s, int val)
 {
-	s->val = val;
+	s->sem_val = val;
 }
 
 #endif
